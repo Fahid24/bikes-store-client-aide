21320c46471c4e1e8700eea36deb44f79520587b
Fahid24 <fahidhasanfuad20018@gmail.com>
2022-05-09 10:23:36 +0600

    added extres

--- src/componants/Pages/Home/Home/Home.js
+++ src/componants/Pages/Home/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../../../../Shared/Footer/Footer';
 import Banner from '../Banner/Banner';
 import DemoProducts from '../DemoProducts/DemoProducts';
+import Search from '../../../Search/Search'
 
 const Home = () => {
     return (
         <div >
             <Banner></Banner>
+            <Search></Search>
             <DemoProducts></DemoProducts>
             <Footer></Footer>
         </div>
